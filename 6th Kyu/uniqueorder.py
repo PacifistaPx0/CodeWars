bbb828bddc4e2c2e0c62ee19f3a0c1627e035577
PacifistaPx0 <richieadigwe@gmail.com>
2021-07-02 19:30:58 +0100

    minor change

--- 6th Kyu/uniqueorder.py
+++ 6th Kyu/uniqueorder.py
@@ -1,4 +1,4 @@
-#function unique_in_order which takes as argument
+#function unique_in_order which takes an argument
 #a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 def unique_in_order(iterable): 
